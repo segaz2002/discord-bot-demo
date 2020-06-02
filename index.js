1d6f1057461864e39f8d06dd9d04f58a2a012cd0
Gabriel <segaz2002@gmail.com>
2020-06-03 02:09:32 +0300

    message handler

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const handle_message = (msg) => {
+  console.log(`Incoming message > ${msg.content}`);
+  let response = null;
+
+  switch (msg.content) {
+    case 'Hello':
+      response = 'Hi';
+      break;
+    case 'Tere':
+      response = 'kuidas';
+      break;
+    default:
+      response = 'Sorry I cannot handle this message yet';
+  }
+
+  msg.reply(response);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  console.log(msg.author);
+  if (!msg.author.bot) {
+    handle_message(msg);
+  }
+});
+
+client.login("your-token");
